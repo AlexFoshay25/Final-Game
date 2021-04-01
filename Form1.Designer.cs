6b54a37c3f52ecfbc3bf21d949a00ca8d8cf21b6
Alex Foshay <FoshAlex157@GoTVDSB.ca>
2021-04-01 12:54:46 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Final_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.waldoHitbox = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pbWaldoHitbox = new System.Windows.Forms.PictureBox();
+            this.pbWaldoPage = new System.Windows.Forms.PictureBox();
+            this.pbEvilWaldoHitbox = new System.Windows.Forms.PictureBox();
+            this.pbWizardHitbox = new System.Windows.Forms.PictureBox();
+            this.pbWilmaHitbox = new System.Windows.Forms.PictureBox();
+            this.lblTask = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWaldoHitbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWaldoPage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEvilWaldoHitbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWizardHitbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWilmaHitbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // waldoHitbox
+            // 
+            this.waldoHitbox.AutoSize = true;
+            this.waldoHitbox.BackColor = System.Drawing.Color.Transparent;
+            this.waldoHitbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.waldoHitbox.ForeColor = System.Drawing.Color.Transparent;
+            this.waldoHitbox.Location = new System.Drawing.Point(362, 13);
+            this.waldoHitbox.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.waldoHitbox.Name = "waldoHitbox";
+            this.waldoHitbox.Size = new System.Drawing.Size(19, 13);
+            this.waldoHitbox.TabIndex = 2;
+            this.waldoHitbox.Text = "....";
+            this.waldoHitbox.Click += new System.EventHandler(this.waldoHitbox_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(369, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(170, 27);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Where\'s Waldo?";
+            // 
+            // pbWaldoHitbox
+            // 
+            this.pbWaldoHitbox.BackColor = System.Drawing.Color.Transparent;
+            this.pbWaldoHitbox.Location = new System.Drawing.Point(365, 119);
+            this.pbWaldoHitbox.Name = "pbWaldoHitbox";
+            this.pbWaldoHitbox.Size = new System.Drawing.Size(30, 49);
+            this.pbWaldoHitbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWaldoHitbox.TabIndex = 5;
+            this.pbWaldoHitbox.TabStop = false;
+            this.pbWaldoHitbox.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pbWaldoPage
+            // 
+            this.pbWaldoPage.Image = global::Final_Game.Properties.Resources.Waldo;
+            this.pbWaldoPage.Location = new System.Drawing.Point(12, 46);
+            this.pbWaldoPage.Name = "pbWaldoPage";
+            this.pbWaldoPage.Size = new System.Drawing.Size(878, 521);
+            this.pbWaldoPage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWaldoPage.TabIndex = 1;
+            this.pbWaldoPage.TabStop = false;
+            // 
+            // pbEvilWaldoHitbox
+            // 
+            this.pbEvilWaldoHitbox.BackColor = System.Drawing.Color.Transparent;
+            this.pbEvilWaldoHitbox.Location = new System.Drawing.Point(170, 409);
+            this.pbEvilWaldoHitbox.Name = "pbEvilWaldoHitbox";
+            this.pbEvilWaldoHitbox.Size = new System.Drawing.Size(30, 49);
+            this.pbEvilWaldoHitbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEvilWaldoHitbox.TabIndex = 6;
+            this.pbEvilWaldoHitbox.TabStop = false;
+            this.pbEvilWaldoHitbox.Click += new System.EventHandler(this.pbEvilWaldoHitbox_Click);
+            // 
+            // pbWizardHitbox
+            // 
+            this.pbWizardHitbox.BackColor = System.Drawing.Color.Transparent;
+            this.pbWizardHitbox.Location = new System.Drawing.Point(597, 46);
+            this.pbWizardHitbox.Name = "pbWizardHitbox";
+            this.pbWizardHitbox.Size = new System.Drawing.Size(30, 40);
+            this.pbWizardHitbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWizardHitbox.TabIndex = 7;
+            this.pbWizardHitbox.TabStop = false;
+            this.pbWizardHitbox.Click += new System.EventHandler(this.pbWizardHitbox_Click);
+            // 
+            // pbWilmaHitbox
+            // 
+            this.pbWilmaHitbox.BackColor = System.Drawing.Color.Transparent;
+            this.pbWilmaHitbox.Location = new System.Drawing.Point(259, 409);
+            this.pbWilmaHitbox.Name = "pbWilmaHitbox";
+            this.pbWilmaHitbox.Size = new System.Drawing.Size(33, 49);
+            this.pbWilmaHitbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbWilmaHitbox.TabIndex = 8;
+            this.pbWilmaHitbox.TabStop = false;
+            this.pbWilmaHitbox.Click += new System.EventHandler(this.pbWilmaHitbox_Click);
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTask.Location = new System.Drawing.Point(607, 11);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(273, 15);
+            this.lblTask.TabIndex = 9;
+            this.lblTask.Text = "Task: Find Waldo, Evil Waldo, Wizard, and Wilma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 579);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.pbWilmaHitbox);
+            this.Controls.Add(this.pbWizardHitbox);
+            this.Controls.Add(this.pbEvilWaldoHitbox);
+            this.Controls.Add(this.pbWaldoHitbox);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.waldoHitbox);
+            this.Controls.Add(this.pbWaldoPage);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Making a Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWaldoHitbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWaldoPage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEvilWaldoHitbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWizardHitbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWilmaHitbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbWaldoPage;
+        private System.Windows.Forms.Label waldoHitbox;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pbWaldoHitbox;
+        private System.Windows.Forms.PictureBox pbEvilWaldoHitbox;
+        private System.Windows.Forms.PictureBox pbWizardHitbox;
+        private System.Windows.Forms.PictureBox pbWilmaHitbox;
+        private System.Windows.Forms.Label lblTask;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pbWaldoPage.BackColor = Color.Transparent;
+
+            
+
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbWaldoHitbox.Location = new Point(pbWaldoHitbox.Location.X - pbWaldoPage.Location.X, pbWaldoHitbox.Location.Y - pbWaldoPage.Location.Y);
+            pbWaldoHitbox.Parent = pbWaldoPage;
+
+            pbEvilWaldoHitbox.Location = new Point(pbEvilWaldoHitbox.Location.X - pbWaldoPage.Location.X, pbEvilWaldoHitbox.Location.Y - pbWaldoPage.Location.Y);
+            pbEvilWaldoHitbox.Parent = pbWaldoPage;
+
+            pbWizardHitbox.Location = new Point(pbWizardHitbox.Location.X - pbWaldoPage.Location.X, pbWizardHitbox.Location.Y - pbWaldoPage.Location.Y);
+            pbWizardHitbox.Parent = pbWaldoPage;
+
+            pbWilmaHitbox.Location = new Point(pbWilmaHitbox.Location.X - pbWaldoPage.Location.X, pbWilmaHitbox.Location.Y - pbWaldoPage.Location.Y);
+            pbWilmaHitbox.Parent = pbWaldoPage;
+
+
+
+
+
+        }
+
+        private void waldoHitbox_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "U win";
+        }
+
+        private void btnWaldo_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("clicked");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Waldo Found!";
+
+            
+
+            pbWaldoHitbox.Image = Properties.Resources.BigRedCircle;
+
+        }
+
+        private void pbEvilWaldoHitbox_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Evil Waldo Found!";
+
+            pbEvilWaldoHitbox.Image = Properties.Resources.BigRedCircle;
+            
+        }
+
+        private void pbWizardHitbox_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "The Wizard Found!";
+
+            pbWizardHitbox.Image = Properties.Resources.BigRedCircle;
+        }
+
+        private void pbWilmaHitbox_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Wilma Found!";
+
+            pbWilmaHitbox.Image = Properties.Resources.BigRedCircle;
+        }
+    }
+}
